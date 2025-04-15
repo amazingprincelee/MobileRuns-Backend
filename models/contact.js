eb2b7385387b01f32aac0ea79fba3ebdf5259a69
Prince Lee <prince_lee.nna85@ymail.com>
2025-04-15 09:00:31 +0100

    update waitlist enum

--- models/contact.js
+++ models/contact.js
@@ -9,7 +9,7 @@ const waitListSchema = new mongoose.Schema({
     location: String,
     role: {
         type: String,
-        enum: ["client", "other", "house cleaner", "house helper","care giver","lawyer", "plumber", "electrician", "waste collector", "waste-collector", "mason man", "iron bender", "capenter", "gmp man", "solar installer", "dstv & gotv installer", "cable satellite installer", "laborer", "mechanic", "welder"]
+        enum: ["client", "other", "house cleaner", "house helper","care giver","lawyer", "plumber", "electrician", "waste collector", "waste_collector", "mason man", "iron bender", "capenter", "gmp man", "solar installer", "dstv & gotv installer", "cable satellite installer", "laborer", "mechanic", "welder"]
     },
     createdAt: {type: Date, default: Date.now}
 });
