9d46e2007f68740fc328c3be1caf001d57eea5fd
Prince Lee <prince_lee.nna85@ymail.com>
2025-04-15 13:21:44 +0100

    add toLowerCase to role

--- models/contact.js
+++ models/contact.js
@@ -1,20 +1,44 @@
 import mongoose from "mongoose";
 
-
-
 const waitListSchema = new mongoose.Schema({
-    fullName: String,
-    email: String,
-    phone: String,
-    location: String,
-    role: {
-        type: String,
-        enum: ["client", "other", "house cleaner", "house helper","care giver","lawyer", "plumber", "electrician", "waste collector", "waste_collector", "mason man", "iron bender", "capenter", "gmp man", "solar installer", "dstv & gotv installer", "cable satellite installer", "laborer", "mechanic", "welder"]
-    },
-    createdAt: {type: Date, default: Date.now}
+  fullName: String,
+  email: String,
+  phone: String,
+  location: String,
+  role: {
+    type: String,
+    enum: [
+      "client",
+      "other",
+      "house cleaner",
+      "house_cleaner",
+      "house helper",
+      "house_helper",
+      "care giver",
+      "care_giver",
+      "lawyer",
+      "plumber",
+      "electrician",
+      "waste collector",
+      "waste_collector",
+      "mason man",
+      "mason_man",
+      "iron bender",
+      "iron_bender",
+      "capenter",
+      "gmp man",
+      "gmp_man",
+      "solar installer",
+      "solar_installer",
+      "dstv_&_gotv_installer",
+      "cable_satellite_installer",
+      "laborer",
+      "mechanic",
+      "welder",
+    ],
+  },
+  createdAt: { type: Date, default: Date.now },
 });
-
-
 
 const WaitList = mongoose.model("WaitList", waitListSchema);
 
